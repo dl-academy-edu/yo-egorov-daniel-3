47dc858fd03f357721a9bbd335fe7562fa2264aa
DanilEgorov <114000999+DanilEgorov@users.noreply.github.com>
2023-02-08 21:28:26 +0400

    Домашка

--- app.js
+++
@@ -1,9 +0,0 @@
-import './utils.js';
-import './local.js';
-import './sliderbanner.js';
-import './burgermenu.js';
-import './index.js';
-
-window.addEventListener('DOMContentLoaded', function () {
-
-});
