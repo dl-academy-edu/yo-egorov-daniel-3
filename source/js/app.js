1d5a9c7039d6a16699c48c4f0c82af56b83e73b8
DanilEgorov <114000999+DanilEgorov@users.noreply.github.com>
2023-03-17 10:32:51 +0400

    проект

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,19 @@
-window.addEventListener('DOMContentLoaded', function () {
 
-});
+// for (let i = 0; i < 3; i++) 
+//     while(i*3); {
+// console.log (i);
+//     }
+  
+// for (let i = 0; i===3; Math.pow(3,2)) {
+//     console.log(i);
+// }
+
+
+function pow(a, b) {
+    let result = a;
+    for (let i = 1; i < b; i++) {
+      result *= a;
+    }
+    return result;
+  }
+
